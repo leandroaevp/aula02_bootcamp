1358e66530009a99a896db61603157e32bb1ddd6
Leandro <546585@ead.ufscar.br>
2025-01-15 04:38:17 -0300

    salvando minha aula

---
+++ aula_02.py
@@ -0,0 +1,71 @@
+numero_1 = int(input("Digite um número: "))
+numero_2 = int(input("Digite outro número: "))
+numero_1 + numero_2
+numero = int(input("Digite um numero: "))
+resto = numero//5
+print(resto)
+
+
+
+# multiplicação 
+
+n1 = int(input("Digite um número: "))
+n2 = int(input("Digite outro número: "))
+multi = n1 * n2
+print(multi)
+
+
+# divisão inteira 
+
+n3 = int(input("Digite um número: "))
+n4 = int(input("Digite outro número: "))
+divi = n3%n4
+print(divi)
+
+
+
+# Quadrado
+
+n5 = int(input("Digite um número: "))
+quadr = n5**2
+print(quadr)
+
+
+# Esccreva um programa que receba dois numeros flutuantes e realize sua adição
+n1 = float(input("Digite um numero: "))
+n2 = float(input("Digite outro numero: "))
+soma = n1+n2
+print(soma)
+
+# Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
+n1 = float(input("Digite um numero: "))
+n2 = float(input("Digite outro numero: "))
+media = (n1+n2)/2
+print(media)
+
+# Potência de um número 
+base = int(input("Digite um numero: "))
+expoente = int(input("Digite outro numero: "))
+potencia = base**2
+print(potencia)
+
+# Conversão de Celsiu para Fahrenheit
+celsius = float(input("Digite a temperatura em graus celsius: "))
+Fahrenheit = float((celsius*9/5)+32)
+print(Fahrenheit)
+
+# Calcule a área do circulo recebendo o raio como entrada
+raio = float(input("Digite o raio: "))
+area_do_circulo = (3.14159) * raio**2
+print(area_do_circulo)
+
+
+# Converte string para maiúscula 
+nome = input("Digite seu  nome: ")
+nome_maiuscula = nome.upper()
+print(nome_maiuscula)
+
+# Converte string para minuscula
+nome = input("Digite seu  nome: ")
+nome_minuscula = nome.lower()
+print(nome_minuscula)
