6d7fbd98c8b9d5785b9f6b65b315ccc35bcc6ae0
Leandro dos Santos <546585@ead.ufscar.br>
2025-08-04 21:16:44 -0300

    adicionando exercicios

---
+++ exercicios.py
@@ -0,0 +1,11 @@
+#Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+numero_1= int(input("Insira um  um numero inteiro: "))
+numero_2= int(input("Insira outro numero inteiro: "))
+soma = numero_1+numero_2
+print("A soma dos numéros é : ", soma)
+
+
+#Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+numero = int(input("Digite um numero: "))
+resto_divisao = numero%5
+print("o resto da visão é :", resto_divisao)
